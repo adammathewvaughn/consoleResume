7366e8351cc910c20eb810be5028b876384f37dd
Adam Vaughn <adammathewvaughn>
2021-05-12 19:37:55 -0500

    ready to go

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-function displayPosition() {
 
+console.log("Adam Vaughn".toUpperCase());
+console.log("Career: Quarter Stack Developer");
+console.log("Description: A man in over his head");
+console.log("\n");
+console.log("Interests");
+console.log("* coding");
+console.log("* television");
+console.log("* hiking");
+console.log("* music");
+console.log("\n")
+console.log("Previous experience:");
+displayPosition("Brick Crew", "Cheap labor", "Toted heavy things");
+displayPosition("Steel Mill", "Not so cheap labor", "Burned myself for money");
+displayPosition("Cable Company", "Cheap labor", "Signal-slinger");
+console.log("\n")
+console.log("My skills:");
+displaySkill("*coding*", true);
+displaySkill("hang-gliding", true);
+displaySkill("guitar", false);
+displaySkill("nose-picking", true);
+displaySkill("square-dancing", false);
+displaySkill("yoga", false);
+
+function displayPosition(company, job, description) {
+  console.log("* " + job + " at " + company + " : " + description);
 }
-console.log();
-
-function displaySkill() {
-
+function displaySkill(skill, isCool) {
+  if (isCool) {
+      console.log("* BAM: " + skill);
+  } else {
+      console.log("* " + skill);
+  }
 }
-console.log();
